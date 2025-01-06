af1c7bb3bcc636fe639fa68bb46db25bb71ed32b
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-01-06 15:29:38 +0600

    B_S

---
+++ Binary Search/First Occurrence.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//_______________________________________________________________________________________________
+
+const   int                                     mod = 1e9 + 7;
+#define pi                                      3.141592653
+#define ll                                      long long int
+#define vi                                      vector<ll>
+#define all(x)                                  x.begin(),x.end()
+#define tc                                      ll ttc; cin >> ttc; while(ttc--)
+#define IOS                                     (ios_base :: sync_with_stdio(false), cin.tie(NULL));
+
+void    in(ll &x)                               { cin >> x; }
+void    in(ll &x, ll &y)                        { cin >> x >> y; }
+void    in(ll &x, ll &y, ll &z)                 { cin >> x >> y >> z; }
+void    in(ll &x, ll &y, ll &z, ll &a)          { cin >> x >> y >> z >> a; }
+void    in(ll &x, string &y)                    { cin >> x >> y; }
+void    in(string &x)                           { cin >> x; }
+void    in(ll &x, string &y, string &z)         { cin >> x >> y >> z; }
+void    in(string &x, string &y)                { cin >> x >> y; }
+void    in(string &x, string &y, string &z)     { cin >> x >> y >> z; }
+void    in(vector<ll> &v) { for(auto &it: v)    { cin >> it; }}
+
+void    out()                                   { cout << "\n"; }
+void    out(int x)                              { cout << x << "\n"; }
+void    out(ll x)                               { cout << x << "\n"; }
+void    out(char x)                             { cout << x << "\n"; }
+void    out(double x)                           { cout << x << "\n"; }
+void    out(string x)                           { cout << x << "\n"; }
+void    out(int x, int y)                       { cout << x << " " << y << "\n"; }
+void    out(ll x, ll y)                         { cout << x << " " << y << "\n"; }
+void    out(ll x, string y)                     { cout << x << " " << y << "\n"; }
+void    out(vector<ll> v)                       { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<char> v)                     { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<int> v)                      { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+void    out(vector<string> v)                   { for(auto it: v){ cout << it << " "; } cout << "\n"; }
+
+
+ll gcd(ll a, ll b) {
+    return __gcd(a, b);
+}
+
+ll lcm(ll a, ll b) {
+    return ((a / __gcd(a, b)) * b);
+}
+
+
+//_______________________________________________________________________________________________
+
+void solve() {
+    ll n, k; in(n, k);
+    vector<ll> arr(n); in(arr); sort(all(arr));
+
+    ll l = 0, r = n-1, index;
+    while(l <= r) {
+        ll mid = (l + r) / 2;
+
+        if(arr[mid] == k) {
+            index = mid;
+
+            r = mid - 1;
+        } else if(arr[mid] < k) {
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+
+    cout << "Fast Occurence of " << k << " in " << index + 1 << " th Index" << endl;
+}
+
+//_______________________________________________________________________________________________
+
+
+int main(){
+    IOS;
+    /*
+    tc
+    */
+      solve();
+    return 0;
+}
+
+
+//_______________________________________________________________________________________________
