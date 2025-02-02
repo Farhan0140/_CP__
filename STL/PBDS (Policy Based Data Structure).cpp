999dab13aa82113b133c2ff84a5f208d3b18bbf9
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-02-02 20:30:57 +0600

    Enhance PBDS example in main function with detailed queries and outputs

--- STL/PBDS (Policy Based Data Structure).cpp
+++ STL/PBDS (Policy Based Data Structure).cpp
@@ -27,27 +27,56 @@ template <typename T> using pbds = tree<T, null_type, less<T>,      rb_tree_tag,
 //_______________________________________________________________________________________________
 
 
-int main(){
-    ll n; 
-    cin >> n;
+int main() {
+	pbds<ll> A; // declaration
 
-// Input:
+	// Inserting elements - 1st query
+	A.insert(1);
+	A.insert(10);
+	A.insert(2);
+	A.insert(7);
+	A.insert(2);  
 
-    pbds<ll> pd;
+	// A contains
+	cout << "A = ";
+	for (auto i : A)
+		cout << i << " ";
+	cout << endl;
+	cout << endl;
 
-    for(int i=0; i<n; i++) {
-        ll x; 
-        cin >> x;         // 10
-                          // 1 7 2 9 3 5 8 2 2 2
-        pd.insert(x);
-    }
+	// finding kth element - 4th query
+	cout << "0th element: " << *A.find_by_order(0) << endl;
+	cout << "1st element: " << *A.find_by_order(1) << endl;
+	cout << "2nd element: " << *A.find_by_order(2) << endl;
+	cout << "3rd element: " << *A.find_by_order(3) << endl;
+	cout << endl;
 
-// Output
+	// finding number of elements smaller than X - 3rd query
+	cout << "No. of elems smaller than 6: " << A.order_of_key(6) << endl; // 2
+	cout << "No. of elems smaller than 11: " << A.order_of_key(11) << endl; // 4
+	cout << "No. of elems smaller than 1: " << A.order_of_key(1) << endl; // 0
+	cout << endl;
 
-    for(auto it: pd) {
-        cout << it << " "; // 1 2 3 5 7 8 9 -> Like Set
-    }                      // 1 2 2 2 2 3 5 7 8 9 ->  like multi_set
-    return 0;
+	// lower bound -> Lower Bound of X = first element >= X in the set
+	cout << "Lower Bound of 6: " << *A.lower_bound(6) << endl;
+	cout << "Lower Bound of 2: " << *A.lower_bound(2) << endl;
+	cout << endl;
+
+	// Upper bound -> Upper Bound of X = first element > X in the set
+	cout << "Upper Bound of 6: " << *A.upper_bound(6) << endl;
+	cout << "Upper Bound of 1: " << *A.upper_bound(1) << endl;
+	cout << endl;
+
+	// Remove elements - 2nd query
+	A.erase(1);
+	A.erase(11); // element that is not present is not affected
+
+	// A contains
+	cout << "A = ";
+	for (auto i : A)
+		cout << i << " ";
+	cout << endl;
+
 }
 
 
