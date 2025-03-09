6a5e9a2bb19aceefcdd5c5b004748b6a7f2ca98e
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-09 20:46:01 +0600

    Fix Build function in Segment Tree to handle leaf nodes correctly

--- Segment_Tree/Segment_Tree_Basic.cpp
+++ Segment_Tree/Segment_Tree_Basic.cpp
@@ -11,6 +11,10 @@ ll arr[SZ], Node[4 * SZ];
 
 void Build (ll node, ll Begin, ll End) {
 
+    if(Begin == End) {
+        Node[node] = arr[Begin];   // OR arr[End];
+    }
+
     ll Mid = (Begin + End) / 2;
     ll Left_Child = 2 * node;
     ll Right_Child = (2 * node) + 1;
