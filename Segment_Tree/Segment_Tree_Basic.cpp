3893d0403bbd0d20bde37e1f9d3b662052d5eb0c
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-09 20:39:14 +0600

    Add Build function to Segment Tree implementation

--- Segment_Tree/Segment_Tree_Basic.cpp
+++ Segment_Tree/Segment_Tree_Basic.cpp
@@ -9,6 +9,14 @@ const int SZ = 1e5 + 9;
 
 ll arr[SZ];
 
+void Build (ll Node, ll Begin, ll End) {
+
+    ll mid = (Begin + End) / 2;
+    ll Left_Child = 2 * Node;
+    ll Right_Child = (2 * Node) + 1;
+
+}
+
 //_______________________________________________________________________________________________
 
 
