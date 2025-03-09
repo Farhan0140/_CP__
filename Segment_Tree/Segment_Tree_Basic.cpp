23d703b278ec6bb6cb10d3b995f4f471982fdce3
___Farhan_Nadim______ <122822790+Farhan0140@users.noreply.github.com>
2025-03-09 20:40:46 +0600

    Fix Build function in Segment Tree to correctly handle child nodes

--- Segment_Tree/Segment_Tree_Basic.cpp
+++ Segment_Tree/Segment_Tree_Basic.cpp
@@ -11,9 +11,12 @@ ll arr[SZ];
 
 void Build (ll Node, ll Begin, ll End) {
 
-    ll mid = (Begin + End) / 2;
+    ll Mid = (Begin + End) / 2;
     ll Left_Child = 2 * Node;
     ll Right_Child = (2 * Node) + 1;
+
+    Build(Left_Child, Begin, Mid);
+    Build(Right_Child, Mid + 1, End);
 
 }
 
